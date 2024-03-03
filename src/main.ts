3f0e604b19e207385cca2c06f9ce728afcfa23da
leave3310 <sampisam3@gmail.com>
2024-03-03 13:38:43 +0800

    feat: 新增 veevalidate 中文錯誤提示以及調整 App.vue 的 router-view 名稱

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import { defineRule, configure } from "vee-validate";
 import * as AllRules from "@vee-validate/rules";
-import { localize } from "@vee-validate/i18n";
+import { localize, setLocale } from "@vee-validate/i18n";
 import zh_TW from "@vee-validate/i18n/dist/locale/zh_TW.json";
 
 import App from "@/App.vue";
@@ -13,12 +13,12 @@ Object.keys(AllRules).forEach((rule) =>
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   defineRule(rule, (AllRules as Record<string, any>)[rule]),
 );
-
 configure({
   generateMessage: localize({
     zh_TW,
   }),
 });
+setLocale("zh_TW");
 
 const app = createApp(App);
 const pinia = createPinia();

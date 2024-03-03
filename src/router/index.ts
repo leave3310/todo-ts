08340071c214231b9feaeca88b951fd5891437bf
leave3310 <sampisam3@gmail.com>
2024-03-03 13:39:40 +0800

    feat: 調整 router 的寫法以及新增對應 enum

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,40 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 
+import {
+  LayoutPath,
+  LayoutName,
+  RoutePath,
+  RouteName,
+} from "@/types/enum/router/index";
+
 const router = createRouter({
-  // 4. Provide the history implementation to use. We
-  // are using the hash history for simplicity here.
   history: createWebHashHistory(),
-  routes: [{ path: "/", component: () => import("@/pages/LoginPage.vue") }], // short for `routes: routes`
+  routes: [
+    {
+      path: LayoutPath.DEFAULT_LAYOUT,
+      name: LayoutName.DEFAULT_LAYOUT,
+      component: () => import("@/views/layouts/DefaultLayout/LayoutBlock.vue"),
+      children: [
+        {
+          path: RoutePath.INDEX,
+          name: RouteName.INDEX,
+          component: () => import("@/views/pages/IndexPage.vue"),
+        },
+      ],
+    },
+    {
+      path: LayoutPath.WITHOUT_HEADER,
+      name: LayoutName.WITHOUT_HEADER,
+      component: () => import("@/views/layouts/WithoutHeader/LayoutBlock.vue"),
+      children: [
+        {
+          path: RoutePath.LOGIN,
+          name: RouteName.LOGIN,
+          component: () => import("@/views/pages/LoginPage.vue"),
+        },
+      ],
+    },
+  ],
   scrollBehavior() {
     return { top: 0 };
   },
---
+++ src/types/enum/router/index.ts
@@ -0,0 +1,18 @@
+export enum LayoutPath {
+  DEFAULT_LAYOUT = "",
+  WITHOUT_HEADER = "/",
+}
+export enum LayoutName {
+  DEFAULT_LAYOUT = "DefaultLayout",
+  WITHOUT_HEADER = "WithoutHeader",
+}
+
+export enum RoutePath {
+  INDEX = "",
+  LOGIN = "login",
+}
+
+export enum RouteName {
+  INDEX = "Index",
+  LOGIN = "Login",
+}

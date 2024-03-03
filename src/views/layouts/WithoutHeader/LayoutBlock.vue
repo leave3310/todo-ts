64c25c7446fa85fe0dd39c3ef2fec097867fab45
leave3310 <sampisam3@gmail.com>
2024-03-03 13:03:53 +0800

    feat: 新增沒有 header 的 layout 以及調整Default layout

--- src/views/layouts/WithoutHeader/LayoutBlock.vue
+++ src/views/layouts/WithoutHeader/LayoutBlock.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
-import HeaderBlock from "@/views/layouts/DefaultLayout/HeaderBlock.vue";
 import EffectWrap from "@/views/layouts/EffectWrap.vue";
 </script>
 
 <template>
   <div class="flex h-dvh flex-col">
-    <HeaderBlock />
-    <main class="flex max-w-container flex-1 flex-col">
+    <main class="mx-auto flex w-full max-w-container flex-1 flex-col">
       <router-view />
     </main>
     <EffectWrap />

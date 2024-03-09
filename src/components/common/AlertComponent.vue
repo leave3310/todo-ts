d2a08899bd5fd1b3d22507ab22cdffeb18e81ef9
leave3310 <sampisam3@gmail.com>
2024-03-09 16:40:51 +0800

    feat: 新增 alertComponent 關閉事件

--- src/components/common/AlertComponent.vue
+++ src/components/common/AlertComponent.vue
@@ -2,12 +2,16 @@
 import { FwbAlert } from "flowbite-vue";
 import { useAlertHandler } from "@/compositions/common/useAlertHandler";
 
-const { alertContent } = useAlertHandler();
+const { alertContent, updateAlertContent } = useAlertHandler();
+const closeAlert = () =>
+  updateAlertContent({
+    isShowAlert: false,
+  });
 </script>
 
 <template>
   <div v-if="alertContent.isShowAlert" class="fixed inset-x-0 p-4">
-    <fwb-alert border closable :type="alertContent.type">
+    <fwb-alert border closable :type="alertContent.type" @close="closeAlert">
       {{ alertContent.content }}
     </fwb-alert>
   </div>
